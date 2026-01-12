61b0af98823624490108eaec8fa8f090dc0065bf
TrencsanszkyPatrik <trencsanszkyp@kkszki.hu>
2026-01-12 08:11:44 +0100

    feladat 9-8

---
+++ TrencsánszkyP_backend/Controllers/ActorsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TrencsánszkyP_backend.Models;
+
+namespace TrencsánszkyP_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActorsController : ControllerBase
+    {
+
+        private readonly CinemadbContext _context;
+        public ActorsController(CinemadbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("feladat9/{actor_name}")]
+        public ActionResult GetActorWithBooks()
+        {
+            var actorName = HttpContext.Request.RouteValues["actor_name"]?.ToString();
+            var actor = _context.Actors
+                .Where(a => a.ActorName == actorName)
+                .Select(a => new
+                {
+                    a.ActorId,
+                    a.ActorName,
+                    Movies = a.Movies.Select(m => new
+                    {
+                        m.MovieId,
+                        m.Title,
+                        m.ReleaseDate,
+                        m.ActorId,
+                        m.FilmTypeId
+                    }).ToList()
+                })
+                .FirstOrDefault();
+
+            if (actor == null)
+            {
+                return NotFound(new {
+                    title = "Not found",
+                    status = 404
+                });
+            }
+            return Ok(actor);
+        }
+
+
+
+    }
+}
---
+++ TrencsánszkyP_backend/Controllers/MoviesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TrencsánszkyP_backend.Models;
+
+namespace TrencsánszkyP_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+
+        private readonly CinemadbContext _context;
+        public MoviesController(CinemadbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("feladat10")]
+        public ActionResult GetAllMovies()
+        {
+            try 
+            {
+                var movies = _context.Movies
+                .Select(m => new
+                {
+                    m.MovieId,
+                    m.Title,
+                    m.ReleaseDate,
+                    m.ActorId,
+                    m.FilmTypeId
+                })
+                .ToList();
+
+                return Ok(movies);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
+            
+        }
+
+
+    }
+}
